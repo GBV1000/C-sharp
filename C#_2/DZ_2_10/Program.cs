584e7a9728cc0de972f01a10e7bade96bdcdc8b1
GBV <test.maxidom@yandex.ru>
2023-01-27 09:56:01 +0300

    DZ_2

---
+++ C#_2/DZ_2_10/Program.cs
@@ -0,0 +1,14 @@
+﻿int num_1 = Convert.ToInt32(Console.ReadLine());
+
+if (num_1 > 9)
+{
+    string index = num_1.ToString();
+    Console.WriteLine($"{index[1]}");
+}
+else
+{
+    Console.WriteLine("введите положительное число, более одного десятка");    
+}
+
+
+
