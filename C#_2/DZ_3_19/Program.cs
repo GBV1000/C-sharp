24f609a8dc6f49d5740db766f309401c88c5e102
GBV <test.maxidom@yandex.ru>
2023-01-30 17:56:12 +0300

    DZ_3-19-21-23

---
+++ C#_2/DZ_3_19/Program.cs
@@ -0,0 +1,20 @@
+﻿int num_1 = Convert.ToInt32(Console.ReadLine());
+
+string index = num_1.ToString();
+string obr = "";
+int lega = index.Length;
+
+
+while (lega >= 1)
+{
+    obr += index[lega-1];
+    lega = lega - 1;
+}
+
+int result = Int32.Parse(obr);
+
+if (num_1 == result)
+{
+    Console.WriteLine($"входящее {num_1}, перевертыш {result}. Итог, является палиндромом");
+}
+
---
+++ C#_2/DZ_3_23/Program.cs
@@ -0,0 +1,24 @@
+﻿int num_1 = Convert.ToInt32(Console.ReadLine());
+
+string index = num_1.ToString();
+string obr = "";
+int lega = index.Length;
+int pd1 = 1;
+
+
+
+while(num_1 >= pd1)
+{
+    if (num_1 > pd1)
+    {
+        obr +=  Math.Pow(pd1,3) + ",";
+        pd1 += 1;
+    }
+    else
+    {
+        obr +=  Math.Pow(pd1,3);
+        pd1 += 1;
+    }
+}
+
+Console.WriteLine(obr);
