61780621062fd52e7fa9b6a13c36d69ca9d74a79
GBV1000 <test.maxidom@yandex.ru>
2023-03-01 11:41:09 +0300

    new code

--- C#_2/Program.cs
+++ C#_2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array0 = {"hello", "2", "world", ":-)"};  
+string[] final = {};
+
+    foreach (var item in array0) {
+        int test = item.Length;        
+        if (item.Length < 4)
+            final = final.Append(item).ToArray();
+            
+    
+        
+    }
+
+Console.WriteLine('[' + String.Join(",", final) + "]");
