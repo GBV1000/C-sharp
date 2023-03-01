cb5d8fedab431b03c2063ea0008e728df7c98e75
GBV1000 <test.maxidom@yandex.ru>
2023-03-01 11:44:23 +0300

    correct code

--- C#_2/Program.cs
+++ C#_2/Program.cs
@@ -1,13 +1,11 @@
 ﻿string[] array0 = {"hello", "2", "world", ":-)"};  
 string[] final = {};
 
-    foreach (var item in array0) {
-        int test = item.Length;        
-        if (item.Length < 4)
-            final = final.Append(item).ToArray();
-            
-    
-        
-    }
+foreach (var item in array0) 
+{
+    int test = item.Length;        
+    if (item.Length < 4)
+        final = final.Append(item).ToArray();
+}
 
 Console.WriteLine('[' + String.Join(",", final) + "]");
